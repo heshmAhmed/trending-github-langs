61804c8629bf77060b4cc8bd895d44cbefa4dad2
heshmAhmed <heshamahmed22226.ha@gmail.com>
2021-08-28 06:23:19 +0200

    avoid potential errors

--- src/main/java/heshmahmed/trendinggithublangs/service/TGLService.java
+++ src/main/java/heshmahmed/trendinggithublangs/service/TGLService.java
@@ -2,8 +2,8 @@ package heshmahmed.trendinggithublangs.service;
 
 import heshmahmed.trendinggithublangs.connect.Connector;
 import heshmahmed.trendinggithublangs.model.Response;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import java.text.SimpleDateFormat;
@@ -13,23 +13,36 @@ import java.util.*;
 @RequiredArgsConstructor
 public class TGLService implements ITGLService{
     private final Connector connector;
-    private final String dateFormat = new SimpleDateFormat("yyyy-MM-dd").format(new Date().getTime() - (60L * 24 * 60 * 60 * 1000));
+    private final String dateFormat = new SimpleDateFormat("yyyy-MM-dd").format(new Date().getTime() - (30L * 24 * 60 * 60 * 1000));
 
     @Override
     public Flux<Response> getTrendingRepos() {
-        return connector.callGithubApi(dateFormat).flatMap(response -> {response.count=response.items.size(); return Flux.just(response);});
+        return connector.callGithubApi(dateFormat).flatMap(
+                response -> {
+                    response.count=response.items.size(); return Flux.just(response);
+                });
     }
 
     @Override
     public Map<String, Response> getTrendingLanguages() {
+        return prepTLResponse(Objects.requireNonNull(this.getTrendingRepos().blockFirst()));
+    }
+
+    /**
+     * Prepare trending languages response
+     * @param trendingRepos Response object
+     * @return Map<String, Response>
+     */
+
+    private Map<String, Response> prepTLResponse(@NonNull Response trendingRepos){
         Map<String, Response> map = new HashMap<>();
-        Objects.requireNonNull(this.getTrendingRepos().blockFirst())
-                .getItems().stream().filter(repository -> repository.getLanguage() != null)
+        trendingRepos.getItems().stream().filter(repository -> repository.getLanguage() != null)
                 .forEach(repository -> Optional.ofNullable(map.get(repository.getLanguage()))
                         .ifPresentOrElse(response -> {
-                            response.items.add(repository); response.count++;},
-                                ()->
-                            map.put(repository.getLanguage(), Response.builder().items(new ArrayList<>(List.of(repository))).count(1).build())));
+                            response.items.add(repository); response.count++;
+                            }, ()->
+                                map.put(repository.getLanguage(),
+                                        Response.builder().items(new ArrayList<>(List.of(repository))).count(1).build())));
         return map;
     }
 }
